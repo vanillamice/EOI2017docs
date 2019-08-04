ef5ee53892b2be7958fb9857a4c981e1ab7314d0
Vanillamice <vanillamice@protonmail.com>
2019-08-04 20:11:46 +0200

    Create mul.cpp

---
+++ mul.cpp
@@ -0,0 +1,20 @@
+//problem B.mul
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    long long M1,M2;
+    cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+    cin >> M1 >> M2;
+    cout << M1 * M2 << endl;
+    }  
+
+
+
+    return 0;
+}
